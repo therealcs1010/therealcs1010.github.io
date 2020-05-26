8ddb6124fee87b0ddf1a27d3733f0a4aa808f213
Sim <simyujie@gmail.com>
2020-05-26 17:09:16 +0800

    Fixed mobile version

---
+++ script.js
@@ -0,0 +1,7 @@
+const spans = document.querySelectorAll('h1 span')
+spans.forEach(span => span.addEventListener('mouseover', function(e) {
+    span.classList.add('animated', 'rubberBand')
+}))
+spans.forEach(span => span.addEventListener('mouseout', function(e) {
+    span.classList.remove('animated', 'rubberBand')
+}))
